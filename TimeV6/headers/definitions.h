9a465f7eb27ac916479c0e5afbccb885ac1cb931
arbiter91268 <kris91268@gmail.com>
2014-08-07 08:23:19 +1200

    Start of V6
    Started TimeV6 - my own version. I am creating a new version to program
    it my way, and along the way, see if I can make some improvements.

---
+++ TimeV6/headers/definitions.h
@@ -0,0 +1,8 @@
+#ifdef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define round((x) >= 0 ? (long)((x)+ 0.5) : (long)((x) - 0.5))
+#define max(a, b) (a >= b ? a : b)
+#define min(a, b) (a <= b ? a : b)
+
+#endif /*DEFINITIONS_H*/

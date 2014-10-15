82ede1e687a015315921a4536a6c6880c1fa9c8a
arbiter91268 <kris91268@gmail.com>
2014-10-15 16:08:47 +1300

    Add autonomous macroes
    Programmed the movement macros needed for the autonomous routine, and
    have added and filled in a process journal which documents the whole
    entire process of the version.

---
+++ ImpulseV1/files/autonomous.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+// moves robot at a b c d speeds for x milliseconds
+void move(int durationMsec, int leftDriveOneSpeed, int leftDriveTwoSpeed, int rightDriveOneSpeed, int rightDriveTwoSpeed)
+{
+	bool goalReached = false;
+	int currentTime;
+	motor[lDrive1] = leftDriveOneSpeed;
+	motor[lDrive2] = leftDriveTwoSpeed;
+	motor[rDrive1] = rightDriveOneSpeed;
+	motor[rDrive2] = rightDriveTwoSpeed;
+	while (currentTime < durationMsec)
+	{
+		wait1Msec(1);
+		currentTime++;
+	}
+	motor[lDrive1] = 0;
+	motor[lDrive2] = 0;
+	motor[rDrive1] = 0;
+	motor[rDrive2] = 0;
+}
+
+task autonomous()
+{
+
+}
--- ImpulseV1/headers/definitions.h
+++ ImpulseV1/headers/definitions.h
@@ -1,4 +1,13 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
+// maths functions
+#define abs(x) (x < 0 ? -x : x)
+
+// movement macros
+#define drive(x, y) (move(x, abs(y), abs(y), abs(y), abs(y)))
+#define driveBack(x, y) (move(x, -abs(y), -abs(y), -abs(y), -abs(y)))
+#define turnLeft(x, y) (move(x, -abs(y), -abs(y), abs(y), abs(y))
+#define turnRight(x, y) (move(x, abs(y), abs(y), -abs(y), -abs(y)))
+
 #endif DEFINITIONS_H

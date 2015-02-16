5daa1e112e7ea882fe92876fba7e62f508687fb7
AED2 <AED2@outlook.co.nz>
2015-02-16 22:14:07 +1300

    Firstrun Update

--- ImpulseV1/files/userfunctions/drivemodes.c
+++ ImpulseV1/files/userfunctions/drivemodes.c
@@ -1,7 +1,15 @@
 void arcadeDrive()
 {
-	leftTrackSpeed =(vexRT[Ch3] + vexRT[Ch1]); 
-	rightTrackSpeed =(vexRT[Ch3] - vexRT[Ch1]); 
+	if (firstrun)
+	{
+		leftTrackSpeed =round((vexRT[Ch3] + vexRT[Ch1])*0.7); 
+		rightTrackSpeed =round((vexRT[Ch3] - vexRT[Ch1])*0.7); 
+	}
+	else
+	{
+		leftTrackSpeed =(vexRT[Ch3] + vexRT[Ch1]); 
+		rightTrackSpeed =(vexRT[Ch3] - vexRT[Ch1]); 
+	}	
 }
 
 void tankDrive()
